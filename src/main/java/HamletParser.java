419d898de741e875c2ac26c8b76acb075c61f543
Sravani Vedam <sravanivedam@zipcoders-MBP-4.localdomain>
2019-03-18 17:57:04 -0400

    submitted

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -36,4 +38,31 @@ public class HamletParser {
         return hamletData;
     }
 
+    public String changeHamletToLeon(String myString){
+        myString = myString.replaceAll("Hamlet","Leon");
+        return myString;
+    }
+
+    public String changeHoratioToHamlet(String myString){
+         myString = myString.replaceAll("Horatio","Tariq");
+        return myString;
+    }
+    public Boolean findHoratio(String sentence){
+        Pattern myPattern = Pattern.compile("Horatio");
+        Matcher m = myPattern.matcher(sentence);
+        if(m.find())
+            return true;
+        else
+            return false;
+
+    }
+    public Boolean findHamlet(String sentence){
+        Pattern myPattern = Pattern.compile("Hamlet");
+        Matcher m = myPattern.matcher(sentence);
+        if(m.find())
+            return true;
+        else
+            return false;
+
+    }
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,17 +16,59 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        //Given
+        HamletParser hamletParser = new HamletParser();
+        String expectedString = "Leon got modified";
+
+        //When
+        String actualString=hamletParser.changeHamletToLeon("Hamlet got modified");
+
+        //Then
+        Assert.assertEquals(expectedString,actualString);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        //Given
+        HamletParser hamletParser = new HamletParser();
+        String expectedString = "Tariq got modified";
+
+        //When
+        String actualString=hamletParser.changeHoratioToHamlet("Horatio got modified");
+
+        //Then
+        Assert.assertEquals(expectedString,actualString);
     }
 
     @Test
     public void testFindHoratio() {
+        //Given
+        HamletParser hamletParser = new HamletParser();
+        Boolean expectedBoolean1  = true;
+        Boolean expectedBoolean2  = false;
+
+        //When
+        Boolean actualBoolean1=hamletParser.findHoratio("Horatio got modified");
+        Boolean actualBoolean2=hamletParser.findHoratio("got modified");
+
+        //Then
+        Assert.assertEquals(expectedBoolean1,actualBoolean1);
+        Assert.assertEquals(expectedBoolean2,actualBoolean2);
     }
 
     @Test
     public void testFindHamlet() {
+        //Given
+        HamletParser hamletParser = new HamletParser();
+        Boolean expectedBoolean1  = true;
+        Boolean expectedBoolean2  = false;
+
+        //When
+        Boolean actualBoolean1=hamletParser.findHamlet("Hamlet got modified");
+        Boolean actualBoolean2=hamletParser.findHamlet("got modified");
+
+        //Then
+        Assert.assertEquals(expectedBoolean1,actualBoolean1);
+        Assert.assertEquals(expectedBoolean2,actualBoolean2);
     }
 }
